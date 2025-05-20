3c74f38cfa3f68e14d17affa0e2174ea1d6d207c
nishantgupta83 <65264123+nishantgupta83@users.noreply.github.com>
2025-05-20 01:15:21 -0700

    Update main.js

--- node-music-site/public/js/main.js
+++ node-music-site/public/js/main.js
@@ -1,1 +1,11 @@
 
+function showSection(sectionId) {
+    // Hide all sections
+    const sections = document.getElementsByClassName('content-section');
+    for(let section of sections) {
+        section.style.display = 'none';
+    }
+    
+    // Show selected section
+    document.getElementById(sectionId).style.display = 'block';
+}
